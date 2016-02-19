54915f9945903ffb04c0934bead1c195a35644dc
Hank Lo <Hlo1453@gmail.com>
2016-02-18 17:53:24 -0800

    Completed stock history page

---
+++ application/models/Stocks.php
@@ -0,0 +1,30 @@
+<?php
+class Stocks extends MY_Model {
+
+    // constructor
+    function __construct() {
+        parent::__construct('stocks', 'Code');
+    }
+
+    function getStocks($stockcode) {
+        $querystring = "SELECT * FROM `movements` WHERE `Code` = '";
+        $querystring .= $stockcode;
+        $querystring .= "' ORDER BY Datetime DESC";
+        $result = $this->db->query($querystring);
+
+        return $result;
+    }
+
+    function getCodes() {        
+        $result = $this->db->query("SELECT COUNT(*) AS `Rows`, `Code` FROM `stocks` GROUP BY `Code` ORDER BY `Code`");
+
+        return $result;
+    }
+
+    function getRecentCode() {
+        $querystring = "SELECT * FROM `movements` WHERE 1 ORDER BY Datetime DESC";
+        $result = $this->db->query($querystring);
+
+        return $result;
+    }
+}
--- application/views/stockpage.php
+++ application/views/stockpage.php
@@ -1,1 +1,4 @@
-Stockpage
+<form method="GET" id="stock-select" action="/stock/getSpecificStock" class="form-inline pull-right">
+	{dropdown}
+</form>
+{table}
